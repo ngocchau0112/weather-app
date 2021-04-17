e0ffd6492d7b021d956c386af5a83ccff6213b69
ngocchau0112 <ngocchau0112@gmail.com>
2021-04-17 14:03:50 +0700

    Cleaned up JS & HTML + Added in show weather features for cities located on menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+//Get current Time
 function currentDate() {
   let now = new Date();
 
   let date = now.getDate();
   let year = now.getFullYear();
   let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let seconds = now.getSeconds();
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
 
   let days = [
     "Sunday",
@@ -40,15 +50,19 @@ function currentDate() {
 
 currentDate();
 
-function showCityName(response) {
+//Get geolocation based on current position
+function showCurrentCity(response) {
   let currentCityName = document.querySelector("#city-name");
+  let currentTemp = document.querySelector("#city-temperature");
+  let currentWeather = document.querySelector("#current-weather");
+  let currentHumidity = document.querySelector("#humidity");
+  let currentWindSpeed = document.querySelector("#wind");
+
   currentCityName.innerHTML = response.data.name;
-
-  let currentCityTemp = document.querySelector("#city-temperature");
-  currentCityTemp.innerHTML = response.data.main.temp;
-
-  let currentCityWeather = document.querySelector("#current-weather");
-  currentCityWeather.innerHTML = response.data.weather[0].description;
+  currentTemp.innerHTML = response.data.main.temp;
+  currentWeather.innerHTML = response.data.weather[0].description;
+  currentHumidity.innerHTML = response.data.main.humidity;
+  currentWindSpeed.innerHTML = response.data.wind.speed;
 
   let tempCityC = Math.round(response.data.main.temp);
   let tempCityF = Math.round((response.data.main.temp * 9) / 5 + 32);
@@ -73,7 +87,7 @@ function currentLocation(position) {
   let weatherAPIKey = "e39c2d2fa97742a130427a035ad0b7a5";
   let weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${weatherAPIKey}`;
 
-  axios.get(weatherAPIUrl).then(showCityName);
+  axios.get(weatherAPIUrl).then(showCurrentCity);
 }
 
 showCurrentLocation();
@@ -86,6 +100,7 @@ currentPositionButton.addEventListener("click", function () {
   navigator.geolocation.getCurrentPosition(currentLocation);
 });
 
+//Input a City and Search
 function outputCity(event) {
   event.preventDefault();
 
@@ -103,32 +118,30 @@ function outputCity(event) {
     let temp = response.data.main.temp;
 
     let currentCityName = document.querySelector("#city-name");
-    currentCityName.innerHTML = response.data.name;
-
     let currentTemp = document.querySelector("#city-temperature");
-    currentTemp.innerHTML = response.data.main.temp;
-
     let currentWeather = document.querySelector("#current-weather");
+    let currentHumidity = document.querySelector("#humidity");
+    let currentWindSpeed = document.querySelector("#wind");
 
+    currentCityName.innerHTML = response.data.name;
+    currentTemp.innerHTML = response.data.main.temp;
     currentWeather.innerHTML = response.data.weather[0].description;
+    currentHumidity.innerHTML = response.data.main.humidity;
+    currentWindSpeed.innerHTML = response.data.wind.speed;
     searchingStatus.innerHTML = ``;
 
     if (city !== undefined) {
-      //let humidity = weather[city].humidity;
       let tempC = Math.round(temp);
       let tempF = Math.round((temp * 9) / 5 + 32);
-
       currentTemp.innerHTML = `${tempC}`;
 
       function changeToF(event) {
         event.preventDefault();
-
         currentTemp.innerHTML = `${tempF}`;
       }
 
       function changeToC(event) {
         event.preventDefault();
-
         currentTemp.innerHTML = `${tempC}`;
       }
 
@@ -151,3 +164,187 @@ function enterCity(event) {
 let form = document.querySelector("#city-form");
 form.addEventListener("submit", outputCity);
 form.addEventListener("keyup", enterCity);
+
+//Show Lisbon Weather
+function showLisbonWeather(event) {
+  event.preventDefault();
+  let city = "lisbon";
+
+  let weatherAPIKey = "e39c2d2fa97742a130427a035ad0b7a5";
+  let weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherAPIKey}`;
+
+  axios.get(weatherAPIUrl).then(function (response) {
+    let temp = response.data.main.temp;
+
+    let currentCityName = document.querySelector("#city-name");
+    let currentTemp = document.querySelector("#city-temperature");
+    let currentWeather = document.querySelector("#current-weather");
+    let currentHumidity = document.querySelector("#humidity");
+    let currentWindSpeed = document.querySelector("#wind");
+
+    currentCityName.innerHTML = response.data.name;
+    currentTemp.innerHTML = response.data.main.temp;
+    currentWeather.innerHTML = response.data.weather[0].description;
+    currentHumidity.innerHTML = response.data.main.humidity;
+    currentWindSpeed.innerHTML = response.data.wind.speed;
+    searchingStatus.innerHTML = ``;
+
+    let tempC = Math.round(temp);
+    let tempF = Math.round((temp * 9) / 5 + 32);
+    currentTemp.innerHTML = `${tempC}`;
+
+    function changeToF(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempF}`;
+    }
+
+    function changeToC(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempC}`;
+    }
+
+    document.querySelector("#Fdegree").addEventListener("click", changeToF);
+    document.querySelector("#Cdegree").addEventListener("click", changeToC);
+  });
+}
+
+let showLisbon = document.querySelector("#lisbon");
+showLisbon.addEventListener("click", showLisbonWeather);
+
+//Show Paris Weather
+function showParisWeather(event) {
+  event.preventDefault();
+  let city = "paris";
+
+  let weatherAPIKey = "e39c2d2fa97742a130427a035ad0b7a5";
+  let weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherAPIKey}`;
+
+  axios.get(weatherAPIUrl).then(function (response) {
+    let temp = response.data.main.temp;
+
+    let currentCityName = document.querySelector("#city-name");
+    let currentTemp = document.querySelector("#city-temperature");
+    let currentWeather = document.querySelector("#current-weather");
+    let currentHumidity = document.querySelector("#humidity");
+    let currentWindSpeed = document.querySelector("#wind");
+
+    currentCityName.innerHTML = response.data.name;
+    currentTemp.innerHTML = response.data.main.temp;
+    currentWeather.innerHTML = response.data.weather[0].description;
+    currentHumidity.innerHTML = response.data.main.humidity;
+    currentWindSpeed.innerHTML = response.data.wind.speed;
+    searchingStatus.innerHTML = ``;
+
+    let tempC = Math.round(temp);
+    let tempF = Math.round((temp * 9) / 5 + 32);
+    currentTemp.innerHTML = `${tempC}`;
+
+    function changeToF(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempF}`;
+    }
+
+    function changeToC(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempC}`;
+    }
+
+    document.querySelector("#Fdegree").addEventListener("click", changeToF);
+    document.querySelector("#Cdegree").addEventListener("click", changeToC);
+  });
+}
+
+let showParis = document.querySelector("#paris");
+showParis.addEventListener("click", showParisWeather);
+
+//Show Sydney Weather
+function showSydneyWeather(event) {
+  event.preventDefault();
+  let city = "sydney";
+
+  let weatherAPIKey = "e39c2d2fa97742a130427a035ad0b7a5";
+  let weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherAPIKey}`;
+
+  axios.get(weatherAPIUrl).then(function (response) {
+    let temp = response.data.main.temp;
+
+    let currentCityName = document.querySelector("#city-name");
+    let currentTemp = document.querySelector("#city-temperature");
+    let currentWeather = document.querySelector("#current-weather");
+    let currentHumidity = document.querySelector("#humidity");
+    let currentWindSpeed = document.querySelector("#wind");
+
+    currentCityName.innerHTML = response.data.name;
+    currentTemp.innerHTML = response.data.main.temp;
+    currentWeather.innerHTML = response.data.weather[0].description;
+    currentHumidity.innerHTML = response.data.main.humidity;
+    currentWindSpeed.innerHTML = response.data.wind.speed;
+    searchingStatus.innerHTML = ``;
+
+    let tempC = Math.round(temp);
+    let tempF = Math.round((temp * 9) / 5 + 32);
+    currentTemp.innerHTML = `${tempC}`;
+
+    function changeToF(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempF}`;
+    }
+
+    function changeToC(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempC}`;
+    }
+
+    document.querySelector("#Fdegree").addEventListener("click", changeToF);
+    document.querySelector("#Cdegree").addEventListener("click", changeToC);
+  });
+}
+
+let showSydney = document.querySelector("#sydney");
+showSydney.addEventListener("click", showSydneyWeather);
+
+//Show San Francisco Weather
+function showSanFWeather(event) {
+  event.preventDefault();
+  let city = "san francisco";
+
+  let weatherAPIKey = "e39c2d2fa97742a130427a035ad0b7a5";
+  let weatherAPIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${weatherAPIKey}`;
+
+  axios.get(weatherAPIUrl).then(function (response) {
+    let temp = response.data.main.temp;
+
+    let currentCityName = document.querySelector("#city-name");
+    let currentTemp = document.querySelector("#city-temperature");
+    let currentWeather = document.querySelector("#current-weather");
+    let currentHumidity = document.querySelector("#humidity");
+    let currentWindSpeed = document.querySelector("#wind");
+
+    currentCityName.innerHTML = response.data.name;
+    currentTemp.innerHTML = response.data.main.temp;
+    currentWeather.innerHTML = response.data.weather[0].description;
+    currentHumidity.innerHTML = response.data.main.humidity;
+    currentWindSpeed.innerHTML = response.data.wind.speed;
+    searchingStatus.innerHTML = ``;
+
+    let tempC = Math.round(temp);
+    let tempF = Math.round((temp * 9) / 5 + 32);
+    currentTemp.innerHTML = `${tempC}`;
+
+    function changeToF(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempF}`;
+    }
+
+    function changeToC(event) {
+      event.preventDefault();
+      currentTemp.innerHTML = `${tempC}`;
+    }
+
+    document.querySelector("#Fdegree").addEventListener("click", changeToF);
+    document.querySelector("#Cdegree").addEventListener("click", changeToC);
+  });
+}
+
+let showSanF = document.querySelector("#sanfrancisco");
+showSanF.addEventListener("click", showSanFWeather);
